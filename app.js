b3b98aace61ff53b88e5c5b8c55ff9d052911ec6
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2021-04-20 15:38:01 -0500

    added user data

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>'
-})
+  template: "<h1>Hello {{firstName}}</h1>",
+  data() {
+    return {
+      firstName: "John",
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+    };
+  },
+});
 
-app.mount('#app');
+app.mount("#app");
